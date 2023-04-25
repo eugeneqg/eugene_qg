f57d6ee1a026f5793b899e0c0349edfd1fd22fa5
Eugene <eugeneqg@Eugenrs-MacBook-Air.local>
2023-04-25 18:35:06 +0800

    small fixes

--- src/components/contacts/contacts.jsx
+++ src/components/contacts/contacts.jsx
@@ -1,6 +1,6 @@
-import Container from "react-bootstrap/esm/Container";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "./contacts.css";
 
 import mail from "./icons/mail.svg";
